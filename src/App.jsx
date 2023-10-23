4af1b72068bd951d14040a62e10287c469b11934
Kingson.Arum <nwajiudo@gmail.com>
2023-10-23 02:58:38 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,8 @@ import Portfolio from "./Portfolio";
 import Contactme from "./Contactme";
 import Footer from "./Footer";
 import "./index.css";
-<<<<<<< HEAD
-=======
 import Favicon from "react-favicon";
 import  favicon from  "./Images/1671114250642.jpg";
->>>>>>> second-main
 
 function App() {
   return (
