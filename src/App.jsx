17f3d375ac9ed8933d47581d9e4f1e3674d8740f
Kingson.Arum <nwajiudo@gmail.com>
2023-10-23 02:25:06 +0100

    did some final touches

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,14 @@ import About from "./About";
 import Portfolio from "./Portfolio";
 import Contactme from "./Contactme";
 import Footer from "./Footer";
-import "./index.css"
+import "./index.css";
+import Favicon from "react-favicon";
+import  favicon from  "./Images/1671114250642.jpg";
 
 function App() {
   return (
     <div>
+     <Favicon url={favicon} />
       <Navbar />
       <HeroSection />
       <Skills />
