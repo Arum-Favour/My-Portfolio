3de27f59cbd8dd4d3152bb9b09aaaeaf6871ad26
Kingson.Arum <nwajiudo@gmail.com>
2023-12-27 14:18:43 +0100

    added the ipAddress tracker app

--- src/ProjectDetails.js
+++ src/ProjectDetails.js
@@ -1,6 +1,7 @@
-import  movewithkings from  "./Images/movewithkings.png";
-import  Huddle from  "./Images/Huddle Screenshot.png";
-import  devprofile from  "./Images/Screenshot 2023-08-07 122622.png";
+import movewithkings from "./Images/movewithkings.png";
+import Huddle from "./Images/Huddle Screenshot.png";
+import devprofile from "./Images/Screenshot 2023-08-07 122622.png";
+import IpTracker from "src/Images/Iptracker.png"
 
 const ProjectDetails = [
   {
@@ -8,22 +9,29 @@ const ProjectDetails = [
     name: "Move With Kings",
     description:
       "A beautiful,responsive and detailed landing page made with bootstrap for a moving company.Responsive on all devices.",
-      projectUrl:"https://arum-favour.github.io/Movewithking/",
+    projectUrl: "https://arum-favour.github.io/Movewithking/",
+  },
+  {
+    image: IpTracker,
+    name: "IP Address Tracker",
+    description:
+      "Unlock the power of geolocation with my IP Address Tracker app, an immersive web application crafted using HTML, CSS, and JavaScript. Blending aesthetic finesse with functionality, this app harnesses the power of Geolocation and Leaflet APIs, enabling users to unravel the physical location of any IP address or domain name.",
+    projectUrl: "https://arum-favour.github.io/IP-Address-Tracker/",
   },
   {
     image: Huddle,
     name: "Huddle community page",
     description:
       "Responsive and eye catching modern website made with adaptable UI components for the Huddle Community.Responsive on all devices.",
-      projectUrl:"https://arum-favour.github.io/Huddle-Landing-Page/",
+    projectUrl: "https://arum-favour.github.io/Huddle-Landing-Page/",
   },
   {
     image: devprofile,
     name: "Developer Portfolio",
     description:
       "A Single page modern developer portfolio website made with HTML,CSS AND JAVASCRIPT.Responsive on all devices.",
-      projectUrl:"https://arum-favour.github.io/Developer-Portfolio/",
-  }
+    projectUrl: "https://arum-favour.github.io/Developer-Portfolio/",
+  },
 ];
 
 export default ProjectDetails;
